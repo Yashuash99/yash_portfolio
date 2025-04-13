33d65257e582c9f78f7fb71231085e571b5d86c3
Yashuash99 <yashureemali@gmail.com>
2025-04-13 22:43:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+// Smooth scroll for nav links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+</script>
